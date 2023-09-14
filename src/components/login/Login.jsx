a5b1563efcc3eff99984572a971f134cbc2eabc9
jamshaidazeemofficial <jamshaid@idevnerds.com>
2023-09-14 21:10:53 +0500

    app level css changes

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Login.module.css";
+
+const LoginComponent = () => {
+  return (
+    <>
+      <div className={`${styles.container} full-page`}>
+        <p>Hello</p>
+      </div>
+    </>
+  );
+};
+
+export default LoginComponent;
